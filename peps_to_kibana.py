08384e1f2c56b5f470dea36ac8b30a55d20a3350
Thibault DUCRET <thibault.ducret@cnes.fr>
2018-10-11 15:27:09 +0200

    Added new script

---
+++ peps_to_kibana.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import peps
+import cProfile
+
+
+def main():
+    results = peps.find_products(collection="S2ST",
+                                 nb_resultats_max=500)
+
+    with open("peps.json", "a") as f:
+        f.write(results)
+
+# Puis faire l'import avec :
+# curl -H 'Content-Type: application/x-ndjson' \
+# -XPOST 'localhost:9200/peps/result/_bulk?pretty' \
+# --data-binary @peps_full.json
+
+cProfile.run('main()')
