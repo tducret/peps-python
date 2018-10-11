f50bcfff8c166b1cd9f279dcb1cd635b33fe3022
Thibault DUCRET <thibault.ducret@cnes.fr>
2018-10-11 15:40:28 +0200

    Added kibana output

--- peps_to_kibana.py
+++ peps_to_kibana.py
@@ -2,6 +2,30 @@
 # -*- coding: utf-8 -*-
 import peps
 import cProfile
+import json
+
+_KIBANA_ATTRIBUTES = ["id",
+                      "title",
+                      "collection",
+                      "acquisition_date",
+                      "platform",
+                      "instrument",
+                      "sensor_mode",
+                      "orbit_number",
+                      "resource_size",
+                      "publication_date",
+                      "cloud_cover",
+                      "orbit_direction",
+                      "ingestion_date"]
+
+
+def result_to_kibana(result):
+    kibana_index = {"index": {"_id": result.id}}
+    kibana_dict = {}
+    for key in _KIBANA_ATTRIBUTES:
+        kibana_dict[key] = getattr(result, key)
+    return ("{}\n{}\n".format(json.dumps(kibana_index),
+                              json.dumps(kibana_dict)))
 
 
 def main():
@@ -9,11 +33,13 @@ def main():
                                  nb_resultats_max=500)
 
     with open("peps.json", "w") as f:
-        f.write(str(results))
+        for result in results:
+            f.write(result_to_kibana(result))
 
 # Puis faire l'import avec :
 # curl -H 'Content-Type: application/x-ndjson' \
 # -XPOST 'localhost:9200/peps/result/_bulk?pretty' \
 # --data-binary @peps_full.json
 
-cProfile.run('main()')
+#cProfile.run('main()')
+main()
