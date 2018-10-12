f19073cdae72a0f3c7730344d3a9d36648fdbc5b
Thibault DUCRET <thibault.ducret@cnes.fr>
2018-10-12 15:07:37 +0200

    Get data for Kibana by day

--- peps_to_kibana.py
+++ peps_to_kibana.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 import peps
 import json
+from datetime import datetime, timedelta
+
+#_START_DATE = "2014-06-14"  # First Sentinel Sat data on 15/06/2014
+_START_DATE = "2018-10-01"
+_END_DATE = datetime.today().strftime("%Y-%m-%d")
 
 _KIBANA_ATTRIBUTES = peps._RESULT_ATTRIBUTES
 
@@ -16,14 +21,30 @@ def result_to_kibana(result):
 
 
 def main():
-    results = peps.find_products(start_date="2018-10-10T00:00:00",
-                                 end_date="2018-10-10T01:59:59",
-                                 nb_resultats_max=100000)
-    print("{} results".format(len(results)))
 
-    with open("peps.json", "w") as f:
-        for result in results:
-            f.write(result_to_kibana(result))
+    start = datetime.strptime(_START_DATE, "%Y-%m-%d")
+    end = datetime.strptime(_END_DATE, "%Y-%m-%d")
+
+    while start < end:
+        day_str = start.strftime("%Y-%m-%d")
+
+        print("Search products for {}".format(day_str))
+
+        results = peps.find_products(
+            start_date=day_str+"T00:00:00",
+            end_date=day_str+"T23:59:59",
+            nb_resultats_max=100000)
+
+        print("{} results for {}".format(len(results),
+                                         day_str))
+
+        if len(results) > 0:
+            filename = "peps_{}.json".format(day_str)
+            with open(filename, "w") as f:
+                for result in results:
+                    f.write(result_to_kibana(result))
+
+        start = start + timedelta(days=1)  # increase day one by one
 
 # Puis faire l'import avec :
 # curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/peps/result/_bulk?pretty' --data-binary @peps_full.json
